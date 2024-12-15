93aaac20be1ffd0b19648c4f62cef91981dffe07
0x1byte <mahdikh6@gmail.com>
2024-12-15 22:30:20 +0330

    Initial commit frontend

---
+++ frontend/app/layout.js
@@ -0,0 +1,26 @@
+import { Geist, Geist_Mono } from "next/font/google";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Github User Search",
+  description: "Search any github user and get their details",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        {children}
+      </body>
+    </html>
+  );
+}
